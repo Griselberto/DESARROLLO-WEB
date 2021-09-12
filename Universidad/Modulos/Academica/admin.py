c7953102b2c3f07a7121c73ebd1f4978b5023601
unknown <77065179@icit.pe.net>
2021-09-11 21:35:02 -0500

    subiendo

---
+++ Universidad/Modulos/Academica/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from Modulos.Academica.models import *
+
+# Register ypur models here
+
+admin.site.register(Carrera)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)
